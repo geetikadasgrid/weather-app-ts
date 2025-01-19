02e22d00e806d8b9c82dcaa90927652176e69c01
Geetika <gdas@griddyamics.com>
2025-01-19 16:24:30 +0530

    new project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
+    </div>
+  );
+};
+
+export default App;
